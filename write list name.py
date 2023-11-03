b27b90fec3564d98172bcedaba5997514da44dc5
Pramadika Egamo <dikaegamo1@gmail.com>
2023-11-04 01:43:03 +0700

    Create write list name.py
    code program ini berfungsi untuk mengenerate nama file citra untuk pendataan di google spreadsheet

---
+++ write list name.py
@@ -0,0 +1,17 @@
+import os
+
+# Direktori folder/file citra yang akan di list
+path = os.listdir("D:\Pramadika\S1 INFORMATIKA\Semester 7\Pengembangan Aplikasi AI\Pertemuan 4\Dataset Plat Nomor\K4 - Plat Putih Mobil")
+
+# Print semua nama file citra
+for i in path:
+    print(i)
+
+
+"""
+----------------------- NOTE -----------------------
+1) Path ubah sesuai direktori mana yang akan di print nama file citranya
+2) Copy output kedalam google spreadsheet untuk melakukan pendataan nama citra
+----------------------- NOTE -----------------------
+
+"""
